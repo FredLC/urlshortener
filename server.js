6e777e6f998b6f393e84630a5aeeab760c826a3d
fredlc <fredlefevre@outlook.com>
2020-11-19 11:00:12 -0500

    Initial implementation of url shortener

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
+let originalUrl;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get(`/api/shorturl/:random`, function (req, res) {
+  res.redirect(originalUrl);
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl/new", function (req, res) {
+  let randomNumber = Math.floor(Math.random() * 1000 + 1);
+  originalUrl = req.body.url;
+  dns.lookup(originalUrl, (err, addresses) => {
+    if (err && originalUrl[0] === "h") {
+      res.json({ original_url: originalUrl, short_url: randomNumber });
+    } else if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      res.json({ original_url: originalUrl, short_url: randomNumber });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
